16a41bcb8e6aed29fc156ac29ffaae3624813c0a
iProDev <chavroka@gmail.com>
2024-01-30 17:18:58 +0330

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static('public', options))
 // #############################################################################
 // Catch all handler for all other request.
 app.use('*', (req,res) => {
+  console.log(req);
   res.json({
       at: new Date().toISOString(),
       method: req.method,
